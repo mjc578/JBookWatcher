bf1119ea1f0810e502c8660069edf0072a4e55b5
Michael Chapman <mjc578@gmail.com>
2019-03-13 02:30:04 -0400

    listener for set button goal frag

--- app/src/main/java/com/example/owner/jbookwatcher/Book.java
+++ app/src/main/java/com/example/owner/jbookwatcher/Book.java
@@ -16,5 +16,5 @@ public class Book {
         return bookTitle;
     }
 
-    
+
 }
--- app/src/main/java/com/example/owner/jbookwatcher/GoalFragment.java
+++ app/src/main/java/com/example/owner/jbookwatcher/GoalFragment.java
@@ -1,11 +1,18 @@
 package com.example.owner.jbookwatcher;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -13,17 +20,75 @@ import android.view.ViewGroup;
  */
 public class GoalFragment extends Fragment {
 
+    Button goalButton;
+    Button cancelButton;
+    Button updateButton;
+    TextView goalText;
 
     public GoalFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goal, container, false);
+
+        View fView = inflater.inflate(R.layout.fragment_goal, container, false);
+        setSetListener(fView);
+
+        goalButton = fView.findViewById(R.id.set_goal_button);
+        cancelButton = fView.findViewById(R.id.cancel_goal_button);
+        updateButton = fView.findViewById(R.id.update_goal_button);
+        goalText = fView.findViewById(R.id.goal_text);
+
+
+        return fView;
+    }
+
+    private void setSetListener(View fView){
+
+        fView.findViewById(R.id.set_goal_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final View dView = View.inflate(getContext(), R.layout.set_goal_dialog,null);
+                final AlertDialog dLog = new AlertDialog.Builder(getContext())
+                        .setView(dView)
+                        .setTitle(R.string.set_new_page_goal)
+                        .setPositiveButton("Set", null)
+                        .setNegativeButton("Cancel", null)
+                        .create();
+
+                dLog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialog) {
+                        Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                        button.setOnClickListener(new View.OnClickListener() {
+
+                            @Override
+                            public void onClick(View view) {
+                                EditText eGoalPrompt = dView.findViewById(R.id.user_input_start_goal);
+                                String goalInput = eGoalPrompt.getText().toString();
+                                if(goalInput.equals("")){
+                                    Toast.makeText(getContext(), "No value was entered", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                                int goalVal = Integer.parseInt(goalInput);
+                                goalButton.setVisibility(View.GONE);
+                                cancelButton.setVisibility(View.VISIBLE);
+                                updateButton.setVisibility(View.VISIBLE);
+
+                                //Dismiss once everything is OK.
+                                dLog.dismiss();
+                            }
+                        });
+                    }
+                });
+
+                dLog.show();
+            }
+        });
+
     }
 
 }
