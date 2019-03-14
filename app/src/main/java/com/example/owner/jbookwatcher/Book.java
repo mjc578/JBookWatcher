7ca0b04ff764df6ceeb9a24db28f64d55de2fe4f
Michael Chapman <mjc578@gmail.com>
2019-03-14 00:04:05 -0400

    book list adapter

--- app/src/main/java/com/example/owner/jbookwatcher/Book.java
+++ app/src/main/java/com/example/owner/jbookwatcher/Book.java
@@ -16,5 +16,31 @@ public class Book {
         return bookTitle;
     }
 
+    public void setBookTitle(String bookTitle){
+        this.bookTitle = bookTitle;
+    }
 
+    public int getPageNum(){
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum){
+        this.pageNum = pageNum;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getStartDate(){
+        return startDate;
+    }
+
+    public void setStartDate(String startDate){
+        this.startDate = startDate;
+    }
 }
---
+++ app/src/main/java/com/example/owner/jbookwatcher/BookListAdapter.java
@@ -0,0 +1,39 @@
+package com.example.owner.jbookwatcher;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class BookListAdapter extends ArrayAdapter<Book> {
+
+    ArrayList<Book> bookList;
+
+    public BookListAdapter(Context context, ArrayList<Book> bookList){
+        super(context, 0, bookList);
+        this.bookList = bookList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.book_list_entry, parent, false);
+        }
+        Book currBook = getItem(position);
+
+        TextView bookNameTV = listItemView.findViewById(R.id.collapsed_book_name);
+        bookNameTV.setText(currBook.getBookTitle());
+
+        TextView pageNumTV = listItemView.findViewById(R.id.collapsed_page_num);
+        pageNumTV.setText(currBook.getPageNum() + "");
+
+
+        return listItemView;
+    }
+}
