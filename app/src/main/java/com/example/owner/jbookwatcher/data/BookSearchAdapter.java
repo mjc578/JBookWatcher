6630e3e76a3327c4c127d6f4504ae95adf849789
Michael Chapman <mjc578@gmail.com>
2019-03-23 17:38:24 -0400

    simplified booksearchadapter

--- app/src/main/java/com/example/owner/jbookwatcher/data/BookSearchAdapter.java
+++ app/src/main/java/com/example/owner/jbookwatcher/data/BookSearchAdapter.java
@@ -15,11 +15,6 @@ import com.example.owner.jbookwatcher.R;
 import java.util.ArrayList;
 
 public class BookSearchAdapter extends ArrayAdapter<Book> {
-    private static class ViewHolder {
-        public ImageView ivCover;
-        public TextView tvTitle;
-        public TextView tvAuthor;
-    }
 
     public BookSearchAdapter(Context context, ArrayList<Book> aBooks) {
         super(context, 0, aBooks);
@@ -30,24 +25,19 @@ public class BookSearchAdapter extends ArrayAdapter<Book> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        final Book book = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
-        ViewHolder viewHolder; // view lookup cache stored in tag
         if (convertView == null) {
-            viewHolder = new ViewHolder();
-            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.book_list_entry, parent, false);
-            viewHolder.ivCover = convertView.findViewById(R.id.entryBookCover);
-            viewHolder.tvTitle = convertView.findViewById(R.id.tvTitle);
-            viewHolder.tvAuthor = convertView.findViewById(R.id.tvAuthor);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.book_list_entry, parent, false);
         }
+        final Book book = getItem(position);
         // Populate the data into the template view using the data object
-        viewHolder.tvTitle.setText(book.getBookTitle());
-        viewHolder.tvAuthor.setText(book.getAuthor());
-        Glide.with(convertView.getContext()).load(book.getCoverUrl()).error(R.drawable.no_cover_avail).into(viewHolder.ivCover);
+        TextView tvTitle = convertView.findViewById(R.id.search_tv_title);
+        TextView tvAuthor = convertView.findViewById(R.id.search_tv_author);
+        ImageView ivCover = convertView.findViewById(R.id.search_book_cover);
+        tvTitle.setText(book.getBookTitle());
+        tvAuthor.setText(book.getAuthor());
+        Glide.with(convertView.getContext()).load(book.getCoverUrl()).error(R.drawable.no_cover_avail).into(ivCover);
         // Return the completed view to render on screen
         return convertView;
     }
