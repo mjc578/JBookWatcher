34879c727827fca256b687c223453ed3e2e86353
Michael Chapman <mjc578@gmail.com>
2019-04-05 17:16:17 -0400

    made list view a shared xml file across all three lists

--- app/src/main/java/com/example/owner/jbookwatcher/fragments/ToReadFrag.java
+++ app/src/main/java/com/example/owner/jbookwatcher/fragments/ToReadFrag.java
@@ -1,7 +1,10 @@
 package com.example.owner.jbookwatcher.fragments;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +16,7 @@ import android.widget.TextView;
 import com.example.owner.jbookwatcher.Book;
 import com.example.owner.jbookwatcher.DateLibrary;
 import com.example.owner.jbookwatcher.R;
-import com.example.owner.jbookwatcher.adapters.BookListAdapter;
+import com.example.owner.jbookwatcher.adapters.BookAdapter;
 import com.example.owner.jbookwatcher.data.BookDbHelper;
 
 import java.util.ArrayList;
@@ -26,32 +29,31 @@ public class ToReadFrag extends Fragment {
 
     private final int LIST_IDENT = 2;
     private ArrayList<Book> bookList;
-    private BookListAdapter bookAdapter;
+    private BookAdapter bookAdapter;
     private ListView list;
     private TextView noBooks;
-    private DateLibrary ul;
+    private DateLibrary ud;
     private BookDbHelper dbHelper;
 
     public ToReadFrag() {
         // Required empty public constructor
     }
 
-    /*
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View crView = inflater.inflate(R.layout.fragment_to_read, container, false);
-        ul = new DateLibrary();
+        View crView = inflater.inflate(R.layout.shared_list_view_book_list, container, false);
+        ud = new DateLibrary();
 
         //instance our database
         dbHelper = new BookDbHelper(getContext());
         //load list of books that belong to this list category
         bookList = dbHelper.getListEntries(LIST_IDENT);
-        bookAdapter = new BookListAdapter(getContext(), bookList);
+        bookAdapter = new BookAdapter(getContext(), bookList);
 
-        list = crView.findViewById(R.id.to_read_list_view);
-        noBooks = crView.findViewById(R.id.to_read_list_text_view);
+        list = crView.findViewById(R.id.shared_list_view);
+        noBooks = crView.findViewById(R.id.shared_list_text_view);
 
         //set list adapter on book list if there are any in db
         if(!bookList.isEmpty()){
@@ -76,8 +78,8 @@ public class ToReadFrag extends Fragment {
                 if(view.findViewById(R.id.selected_book_buttons).getVisibility() == View.GONE){
                     view.findViewById(R.id.selected_book_buttons).setVisibility(View.VISIBLE);
 
+                    setBookDetailListener(view, position);
                     setBookDeleteListener(view, position);
-                    setBookEditListener(view, position);
                     setMoveListener(view, position);
                 }
                 else{
@@ -86,5 +88,48 @@ public class ToReadFrag extends Fragment {
             }
         });
     }
-    */
+
+    private void setBookDetailListener(View view, int position){
+        TextView tvBookDetailButton = view.findViewById(R.id.entry_details_button);
+        tvBookDetailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: open list entry details activity with this book's details
+            }
+        });
+    }
+
+    private void setBookDeleteListener(View view, final int position){
+        TextView tvBookDeleteButton = view.findViewById(R.id.entry_delete_button);
+        tvBookDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                Book b = bookList.remove(position);
+                                dbHelper.deleteBook(b);
+                                list.setAdapter(bookAdapter);
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("Delete this Book?").setPositiveButton("Yes", dialogListener)
+                        .setNegativeButton("No", dialogListener).show();
+
+            }
+        });
+    }
+
+    private void setMoveListener(View view, int position){
+
+    }
+
 }
